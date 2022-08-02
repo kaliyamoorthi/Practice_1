d054efe56e0f097b8385d3ace83bfc7cbffc1ca7
niitagile <43024343+niitagile@users.noreply.github.com>
2022-08-02 10:23:06 +0530

    Add files via upload

---
+++ 02Aug/CookieExample/src/main/java/com/cookieexample/Logout.java
@@ -0,0 +1,39 @@
+package com.cookieexample;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Logout
+ */
+@WebServlet("/Logout")
+public class Logout extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+    		throws ServletException, IOException {
+    	// TODO Auto-generated method stub
+    	doPost(req, resp);
+    }
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+			Cookie cookie=new Cookie("username","");
+			cookie.setMaxAge(0);
+			response.addCookie(cookie);
+			
+			request.getRequestDispatcher("link.html").forward(request, response);
+			
+	
+	}
+
+}
